0aab3cb5435e8637a200a851c9b877c6b01774d4
Vivek <madishetty.vivek@gmail.com>
2019-12-10 19:23:06 -0500

    Employee data

--- app/src/main/java/com/employeemanagement/Activity/AddEmployee.java
+++ app/src/main/java/com/employeemanagement/Activity/AddEmployee.java
@@ -12,3 +12,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.view.ViewGroup;
