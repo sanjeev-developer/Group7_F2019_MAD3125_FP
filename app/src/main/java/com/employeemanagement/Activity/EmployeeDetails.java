d761010ad619636b1a7b1e7fefb77df7538b83b2
Sanjeev <sanjeevgupta9223@gmail.com>
2019-12-10 22:30:17 -0500

    working on recycler view

--- app/src/main/java/com/employeemanagement/Activity/EmployeeDetails.java
+++ app/src/main/java/com/employeemanagement/Activity/EmployeeDetails.java
@@ -4,3 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
