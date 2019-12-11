81fc73a41789fadcea52a27c7d14b5700e57770c
Sanjeev <sanjeevgupta9223@gmail.com>
2019-12-10 21:24:06 -0500

    employeeManagement commit

--- app/src/main/java/com/employeemanagement/Activity/BaseActivity.java
+++ app/src/main/java/com/employeemanagement/Activity/BaseActivity.java
@@ -17,3 +17,8 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+
+import com.employeemanagement.R;
+import com.employeemanagement.Utils.EmployeeData;
+import com.google.android.material.snackbar.Snackbar;
