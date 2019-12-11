4f6847d7a12871dffe30026db053ddd0bfbcf622
Sanjeev <sanjeevgupta9223@gmail.com>
2019-12-10 22:31:15 -0500

    Adapters commit

--- app/src/main/java/com/employeemanagement/Activity/EmployeeDetails.java
+++ app/src/main/java/com/employeemanagement/Activity/EmployeeDetails.java
@@ -11,3 +11,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.employeemanagement.Adapters.DashAdapter;
+import com.employeemanagement.Adapters.VehAdapter;
+import com.employeemanagement.R;
